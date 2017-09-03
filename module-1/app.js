23518838bd33dd7428d0fea6d9bd1a8f4e7e86e3
sugandharuchir <ruchirsugandha@gmail.com>
2017-09-04 01:52:51 +0530

    change

--- module-1/app.js
+++ module-1/app.js
@@ -6,8 +6,18 @@ angular.module('lunchCheck', [] )
 
 LunchController.$inject = ['$scope'];
 function LunchController($scope){
-	$scope.message = "Your lunch menu is ready!";
+	$scope.message = "welcome, order here!";
+
+
+$scope.CheckMenu = function(string){
+	var array = string.split(',');
+	var count = string.split(',').length;
+	// console.log(count);
+	if(array==undefined)
+		$scope.message= "You gotta order something!"
+	else if(count>0 && count<=3)
+	$scope.message = "Enjoy your meal! :)"
+	else
+		$scope.message = "Too much for your appetite. Try again!"
 }
-
-
-})();
+}})(); 
